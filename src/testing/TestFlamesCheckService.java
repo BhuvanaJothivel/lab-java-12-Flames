282ea6aed312d44124bd44513e726f5656bdf43e
Bhuvana Jothivel <58466121+BhuvanaJothivel@users.noreply.github.com>
2020-03-09 10:38:10 +0530

    Update TestFlamesCheckService.java

--- src/testing/TestFlamesCheckService.java
+++ src/testing/TestFlamesCheckService.java
@@ -9,31 +9,22 @@ public class TestFlamesCheckService {
 	FlamesCheckService fcs = new FlamesCheckService();
 	@Test
 	public void testFindFlamesMethod() {
-		String name1 = "bhuvana";
-		String name2 = "abarna";
-		char temp = 'f';
+		String name1 = "Alex";
+		String name2 = "Joylin";
+		char temp = 'a';
 		assertEquals(temp,fcs.findFlames(name1, name2));
-		name1 = "Anand";
-		name2 = "Lavanya";
+		name1 = "Steffe";
+		name2 = "Bobby";
 		temp = 'm';
 		assertEquals(temp,fcs.findFlames(name1, name2));
-		name1 = "bhuvaneswari";
-		name2 = "abarna";
-		temp = 'a';
-		assertEquals(temp,fcs.findFlames(name1, name2));
-		name1 = "priyadharshini";
-		name2 = "karthikeyan";
-		temp = 3;
-		assertEquals(temp,fcs.findFlames(name1, name2));
-		name1 = "bhuvana";
-		name2 = "gayathri";
+		name1 = "John";
+		name2 = "Jully";
 		temp = 'e';
 		assertEquals(temp,fcs.findFlames(name1, name2));
-		name1 = "acxdgjikz";
-		name2 = "b";
+		name1 = "George";
+		name2 = "Neythiri";
 		temp = 'l';
-		assertEquals(temp,fcs.findFlames(name1, name2));
-		
+	    assertEquals(temp,fcs.findFlames(name1, name2));
 		try {
 			fcs.findFlames(null, null);
 			fcs.findFlames("AAA", null);
